fedae3bdacba2a7164c0b8c7afaabf96c6acab6a
jonnysp <jonnyspitzner@googlemail.com>
2017-07-28 08:20:05 +0200

    add Models

---
+++ src/Resources/contao/models/VegasModel.php
@@ -0,0 +1,6 @@
+<?php
+
+class VegasModel extends \Model
+{
+    protected static $strTable = 'tl_vegas';
+}
---
+++ src/Resources/contao/models/VegasSlidesModel.php
@@ -0,0 +1,6 @@
+<?php
+
+class VegasSlidesModel extends \Model
+{
+    protected static $strTable = 'tl_vegas_slides';
+}
